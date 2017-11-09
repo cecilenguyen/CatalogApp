3e1ff5e96b8941f7dce3493c53ca7089bc1f6f9c
vagrant <vagrant@vagrant.vm>
2017-11-09 04:48:57 +0000

    home template and routes for getting all categories and latest items

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # create flask app
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # add database to application
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item
 
@@ -18,27 +18,31 @@ session = DBSession()
 @app.route('/catalog')
 def getCatalog():
     category = session.query(Category)
-    output = ''
-    for cat in category:
-        output += cat.name
-        output += '</br>'
-        items = session.query(Item).filter_by(category_id = cat.id)
-        for i in items:
-            output += i.name
-            output += '</br>'
-        output += '</br>'
-    return output
-        
+    item = session.query(Item).order_by(desc(Item.date)).limit(5)
+    return render_template('homepage.html', category=category, item=item)
 
 # get all items for specific category
-@app.route('/catalog/<path:category_name>')
+@app.route('/catalog/<path:category>')
 def getCategory(category):
-    print()
+    category = session.query(Category).filter_by(name=category).first()
+    items = session.query(Item).filter_by(category_id = category.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+    return output
+    
 
 # get single item
 @app.route('/catalog/<path:category>/<path:item>')
 def getItem(category, item):
-    print()
+    category = session.query(Category).filter_by(name=category).first()
+    item = session.query(Item).filter_by(category_id=category.id, name=item).first()
+    output = ''
+    output += item.name + '</br>'
+    output += item.description
+    return output
+
 
 ####### Add routes #######
 
@@ -46,11 +50,21 @@ def getItem(category, item):
 @app.route('/catalog/addCategory', methods=['GET','POST'])
 def addCategory():
     print()
+    # category = Category(id=1, name="Soccer")
+    # session.add(category)
+    # session.commit()
 
 # add an item
 @app.route('/catalog/addItem', methods=['GET','POST'])
 def addItem():
     print()
+    # item5 = Item(
+    #             name="Jersey",
+    #             category_id="2",
+    #             description="Limited edition jersey for local team.",
+    #             date=datetime.datetime.now())
+    # session.add(item5)
+    # session.commit()
 ####### Add routes #######
 
 
