14979e73fc001b11bad0283514fa962ab221d39d
vagrant <vagrant@vagrant.vm>
2017-11-17 19:02:10 +0000

    login and logout works

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from flask import make_response
 # add database to application
 from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Category, Item
+from database_setup import Base, Category, Item, User
 
 app = Flask(__name__)
 
@@ -133,10 +133,8 @@ def gconnect():
 
 # User Helper Functions
 
-
 def createUser(login_session):
-    newUser = User(name=login_session['username'], email=login_session[
-                   'email'], picture=login_session['picture'])
+    newUser = User(name=login_session['username'], email=login_session['email'], picture=login_session['picture'])
     session.add(newUser)
     session.commit()
     user = session.query(User).filter_by(email=login_session['email']).one()
